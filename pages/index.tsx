54f0149997034c93a98df0d453299ede224de7d4
ozaki25 <fs65ugumori@yahoo.co.jp>
2021-02-27 00:45:45 +0900

    show sample data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,12 @@
 import Head from 'next/head';
+import { items } from '../data';
+import { Item } from '../interfaces';
 
-export default function Home() {
+interface HomeProps {
+  items: Item[];
+}
+
+export default function Home({ items }: HomeProps) {
   return (
     <>
       <Head>
@@ -9,7 +15,14 @@ export default function Home() {
       </Head>
       <main>
         <h1>Hello</h1>
+        {items.map(item => (
+          <p key={item.name}>{item.name}</p>
+        ))}
       </main>
     </>
   );
+}
+
+export async function getStaticProps() {
+  return { props: { items } };
 }
