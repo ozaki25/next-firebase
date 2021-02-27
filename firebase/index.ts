6f452e544dbe151cf515991e74b9940e4fd88702
ozaki25 <fs65ugumori@yahoo.co.jp>
2021-02-28 00:51:47 +0900

    update firebase init

--- firebase/index.ts
+++ firebase/index.ts
@@ -1,13 +1,15 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+import * as admin from 'firebase-admin';
 
 const clientCredentials = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  credential: admin.credential.cert({
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  }),
 };
 
-if (!firebase.apps.length) {
-  firebase.initializeApp(clientCredentials);
+if (!admin.apps.length) {
+  admin.initializeApp(clientCredentials);
 }
 
-export default firebase;
+export default admin;
