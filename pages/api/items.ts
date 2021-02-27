05e711c60fc0db6d91dc9bd98287528fe600c224
ozaki25 <fs65ugumori@yahoo.co.jp>
2021-02-28 01:56:40 +0900

    add get items and post item api

--- pages/api/items.ts
+++ pages/api/items.ts
@@ -2,16 +2,38 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import firebase from '../../firebase';
 import { Item } from '../../interfaces';
 
-export default async (req: NextApiRequest, res: NextApiResponse<Item[]>) => {
-  if (req.method === 'GET') {
-    const db = firebase.firestore();
-    const itemsCollection = db.collection('items');
-    const snapshot = await itemsCollection.get();
-    const items = snapshot.docs.map(doc => {
-      const data = doc.data();
-      return { id: doc.id, title: data.title } as Item;
-    });
+export default async function (
+  req: NextApiRequest,
+  res: NextApiResponse<Item[] | Item | string>,
+) {
+  console.log(req.method, req.url);
+  if (req.method === 'GET') return get(req, res);
+  if (req.method === 'POST') return post(req, res);
+  res.status(405).json('Method Not Allowed');
+}
 
-    res.status(200).json(items);
-  }
-};
+async function get(req: NextApiRequest, res: NextApiResponse<Item[]>) {
+  const db = firebase.firestore();
+  const snapshot = await db.collection('items').get();
+
+  const items = snapshot.docs.map(doc => {
+    const data = doc.data();
+    return { id: doc.id, title: data.title } as Item;
+  });
+  console.log({ items });
+
+  res.status(200).json(items);
+}
+
+async function post(req: NextApiRequest, res: NextApiResponse<Item>) {
+  console.log({ body: req.body });
+  const body = JSON.parse(req.body) as Item;
+  const db = firebase.firestore();
+  const doc = await db.collection('items').add(body);
+
+  const snapshot = await doc.get();
+  const item = { id: snapshot.id, ...snapshot.data() } as Item;
+  console.log({ item });
+
+  res.status(200).json(item);
+}
