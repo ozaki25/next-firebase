42b922d8f59dbd30863094ed51fea77bdb5fe32e
ozaki25 <fs65ugumori@yahoo.co.jp>
2021-03-02 00:06:27 +0900

    add _app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,22 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
