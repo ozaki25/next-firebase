6ee9788317818ce4aa03a5f6f8ba3b9971ca634e
ozaki25 <fs65ugumori@yahoo.co.jp>
2021-03-02 00:40:45 +0900

    apply mui ssr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
+import { useEffect } from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles && jssStyles.parentElement) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
   return (
     <>
       <Head>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import { Children } from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheets } from '@material-ui/core/styles';
 
 class MyDocument extends Document {
   render() {
@@ -19,5 +20,24 @@ class MyDocument extends Document {
     );
   }
 }
+
+MyDocument.getInitialProps = async ctx => {
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+  ctx.renderPage = () => {
+    return originalRenderPage({
+      enhanceApp: App => props => sheets.collect(<App {...props} />),
+    });
+  };
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    styles: [
+      ...Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  };
+};
 
 export default MyDocument;
