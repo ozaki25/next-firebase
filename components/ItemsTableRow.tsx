bca1f6eb66b81550874dc8eced534a8c6d2bde10
ozaki25 <fs65ugumori@yahoo.co.jp>
2021-03-06 01:59:18 +0900

    add editting button

--- components/ItemsTableRow.tsx
+++ components/ItemsTableRow.tsx
@@ -1,6 +1,13 @@
-import { MouseEvent } from 'react';
+import { MouseEvent, useState } from 'react';
 import { IconButton, TableCell, TableRow } from '@material-ui/core';
-import { Check, Clear, SwapVert } from '@material-ui/icons';
+import {
+  ArrowDownward,
+  ArrowUpward,
+  CancelOutlined,
+  Check,
+  Clear,
+  SwapVert,
+} from '@material-ui/icons';
 
 import { Item } from '../interfaces';
 
@@ -9,25 +16,58 @@ interface Props {
 }
 
 export default function TableItem({ item }: Props) {
-  const onClickSwap = (event: MouseEvent<HTMLButtonElement>) => {
-    alert();
+  const [isEditting, setIsEditing] = useState<boolean>(false);
+
+  const onClickSwapStart = (event: MouseEvent<HTMLButtonElement>) => {
+    setIsEditing(true);
   };
+
+  const onClickSwapCancel = (event: MouseEvent<HTMLButtonElement>) => {
+    setIsEditing(false);
+  };
+
   return (
     <TableRow hover={true}>
       <TableCell>
         <a href={item.url}>{item.title}</a>
       </TableCell>
-      <TableCell align="center" padding="none">
-        {item.flag1 ? <Check color="primary" /> : <Clear color="action" />}
-      </TableCell>
-      <TableCell align="center" padding="none">
-        {item.flag2 ? <Check color="primary" /> : <Clear color="action" />}
-      </TableCell>
-      <TableCell align="center" padding="none">
-        <IconButton aria-label="swap" size="small" onClick={onClickSwap}>
-          <SwapVert />
-        </IconButton>
-      </TableCell>
+      {isEditting ? (
+        <>
+          <TableCell align="center" padding="none">
+            <IconButton aria-label="swap up" size="small">
+              <ArrowUpward color="secondary" />
+            </IconButton>
+          </TableCell>
+          <TableCell align="center" padding="none">
+            <IconButton aria-label="swap down" size="small">
+              <ArrowDownward color="secondary" />
+            </IconButton>
+          </TableCell>
+          <TableCell align="center" padding="none">
+            <IconButton aria-label="cancel" size="small">
+              <CancelOutlined onClick={onClickSwapCancel} />
+            </IconButton>
+          </TableCell>
+        </>
+      ) : (
+        <>
+          <TableCell align="center" padding="none">
+            {item.flag1 ? <Check color="primary" /> : <Clear color="action" />}
+          </TableCell>
+          <TableCell align="center" padding="none">
+            {item.flag2 ? <Check color="primary" /> : <Clear color="action" />}
+          </TableCell>
+          <TableCell align="center" padding="none">
+            <IconButton
+              aria-label="swap"
+              size="small"
+              onClick={onClickSwapStart}
+            >
+              <SwapVert />
+            </IconButton>
+          </TableCell>
+        </>
+      )}
     </TableRow>
   );
 }
